17ec643b594206dda4be40787172686e2427b2d8
John Blum <jblum2@apple.com>
2024-08-12 18:59:36 -0700

    Give Docker Image and Containers meaningful names.

--- bin/docker-commands.sh
+++ bin/docker-commands.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker image build . -t codeprimate/spring-boot-app-a
-docker image build . -t codeprimate/spring-boot-app-b
+docker image build -t codeprimate/spring-boot-app-skullbox .
+docker image build -t codeprimate/spring-boot-app-boombox .
 docker network create boot-net
-docker container run --name skullbox --network boot-net -it -p 7070:7070 codeprimate/spring-boot-app-a
-docker container run --name boombox --network boot-net -it -p 9090:9090 codeprimate/spring-boot-app-b
+docker container run --name skullbox --network boot-net -it -p 7070:7070 codeprimate/spring-boot-app-skullbox
+docker container run --name boombox --network boot-net -it -p 9090:9090 codeprimate/spring-boot-app-boombox
